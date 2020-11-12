c422b6de539704ef01f9e8e7af261886942f36f9
wlee221 <wlee221@live.unc.edu>
2020-11-11 20:06:38 -0800

    Try 3.x.x

--- packages/amplify-ui-components/scripts/update-deps.js
+++ packages/amplify-ui-components/scripts/update-deps.js
@@ -6,10 +6,10 @@ const PACKAGE_JSON_DIR = `./package.json`;
 
 const file = editJsonFile(PACKAGE_JSON_DIR);
 
-file.set('dependencies.@aws-amplify/auth', '^3.0.0');
-file.set('dependencies.@aws-amplify/core', '^3.0.0');
-file.set('dependencies.@aws-amplify/storage', '^3.0.0');
-file.set('dependencies.@aws-amplify/interactions', '^3.0.0');
-file.set('dependencies.@aws-amplify/xr', '^2.0.0');
+file.set('dependencies.@aws-amplify/auth', '3.x.x');
+file.set('dependencies.@aws-amplify/core', '3.x.x');
+file.set('dependencies.@aws-amplify/storage', '3.x.x');
+file.set('dependencies.@aws-amplify/interactions', '3.x.x');
+file.set('dependencies.@aws-amplify/xr', '2.x.x');
 
 file.save();
