15437fe966961f97b59780411c4bf6fc27fcd9a8
wlee221 <wlee221@live.unc.edu>
2020-11-11 17:53:39 -0800

    Add version lifecycle script

---
+++ packages/amplify-ui-components/scripts/update-deps.js
@@ -0,0 +1,12 @@
+const editJsonFile = require('edit-json-file');
+
+const PACKAGE_JSON_DIR = `${__dirname}/package.json`;
+
+const file = editJsonFile(PACKAGE_JSON_DIR);
+
+file.set('dependencies.@aws-amplify/auth', '^3.0.0');
+file.set('dependencies.@aws-amplify/storage', '^3.0.0');
+file.set('dependencies.@aws-amplify/interactions', '^3.0.0');
+file.set('dependencies.@aws-amplify/xr', '^2.0.0');
+
+file.save();
