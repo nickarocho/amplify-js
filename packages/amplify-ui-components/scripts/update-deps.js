b6c31c0cd090e81b77a4d113dd2b135810fea80e
wlee221 <wlee221@live.unc.edu>
2020-11-11 19:08:48 -0800

    Add missing core

--- packages/amplify-ui-components/scripts/update-deps.js
+++ packages/amplify-ui-components/scripts/update-deps.js
@@ -7,6 +7,7 @@ const PACKAGE_JSON_DIR = `./package.json`;
 const file = editJsonFile(PACKAGE_JSON_DIR);
 
 file.set('dependencies.@aws-amplify/auth', '^3.0.0');
+file.set('dependencies.@aws-amplify/core', '^3.0.0');
 file.set('dependencies.@aws-amplify/storage', '^3.0.0');
 file.set('dependencies.@aws-amplify/interactions', '^3.0.0');
 file.set('dependencies.@aws-amplify/xr', '^2.0.0');
